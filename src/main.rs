ee746ad1470ca044a7762df546809ed4a39795d8
Fin Christensen <christensen.fin@gmail.com>
2019-03-05 14:45:54 +0100

    Cleanup API

--- src/main.rs
+++ src/main.rs
@@ -31,10 +31,11 @@ macro_rules! state_machine {
     };
 
     ( @def event $name:ident (
-        $($param:ident : $t:ty),* $(,)?
+        $old:ident : &State ,
+        $new:ident : &State $(,)?
     ) $handler:block ) => {
         impl<'a> Machine<'a> {
-            fn $name($($param : $t),*) {
+            fn $name($old : &State, $new : &State) {
                 $handler
             }
         }
@@ -91,11 +92,11 @@ macro_rules! state_machine {
 
 state_machine! {
     machine BottleFiller {
-        event run(name: String) {
+        event run(old: &State, new: &State) {
             //println!("Run application");
         }
 
-        event pause(name: String, duration: i32) {
+        event pause(old: &State, new: &State) {
             //println!("Pause application");
         }
 
@@ -105,7 +106,7 @@ state_machine! {
             Paused(),
         }
 
-        event stop() {
+        event stop(old: &State, new: &State) {
             //println!("Stop application");
         }
 
@@ -119,110 +120,6 @@ state_machine! {
     }
 }
 
-#[derive(PartialEq, Clone, Debug)]
-enum BottleFillerState {
-    Waiting,
-    Filling { volume: usize },
-    Done,
-}
-
-impl State for BottleFillerState {}
-
 fn main() {
-    let mut transitions = HashMap::new();
-    transitions.insert(
-        "start filling",
-        StateMachineTransition::new(
-            |old_state| {
-                match old_state {
-                    BottleFillerState::Waiting => {
-                        println!("start filling");
-
-                        Some(BottleFillerState::Filling { volume: 250 })
-                    },
-                    _ => None,
-                }
-            },
-        ),
-    );
-    transitions.insert(
-        "stop filling",
-        StateMachineTransition::new(
-            |old_state| {
-                match old_state {
-                    BottleFillerState::Filling { volume: _ } => {
-                        println!("stop filling");
-
-                        Some(BottleFillerState::Done)
-                    },
-                    _ => None,
-                }
-            },
-        ),
-    );
-    transitions.insert(
-        "bottle removed",
-        StateMachineTransition::new(
-            |old_state| {
-                match old_state {
-                    BottleFillerState::Done => {
-                        println!("bottle removed");
-
-                        Some(BottleFillerState::Waiting)
-                    },
-                    _ => None,
-                }
-            },
-        ),
-    );
-
-    let mut sm2 = StateMachine::new(BottleFillerState::Waiting, transitions);
-    assert_eq!(sm2.trigger_event("start filling"), true);
-    assert_eq!(sm2.trigger_event("bottle removed"), false);
-    assert_eq!(sm2.trigger_event("stop filling"), true);
-    assert_eq!(sm2.trigger_event("bottle removed"), true);
-}
-
-trait State: std::cmp::PartialEq + std::clone::Clone {}
-
-struct StateMachineTransition<'a, T> {
-    transition_handler: Box<Fn(&T) -> Option<T> + 'a>,
-}
-
-impl<'a, T: State> StateMachineTransition<'a, T> {
-    fn new<F: Fn(&T) -> Option<T> + 'a>(transition_handler: F) -> Self {
-        StateMachineTransition {
-            transition_handler: Box::new(transition_handler),
-        }
-    }
-}
-
-struct StateMachine<'a, T> {
-    state: T,
-    transitions: HashMap<&'a str, StateMachineTransition<'a, T>>,
-}
-
-impl<'a, T: State> StateMachine<'a, T> {
-    pub fn new(initial_state: T, transitions: HashMap<&'a str, StateMachineTransition<'a, T>>) -> Self {
-        StateMachine {
-            state: initial_state,
-            transitions: transitions,
-        }
-    }
-
-    pub fn trigger_event(&mut self, event: &str) -> bool {
-        let transition_option = self.transitions.get(event);
-
-        if transition_option.is_some() {
-            let transition = transition_option.unwrap();
-            let new_state_option = (transition.transition_handler)(&self.state);
-            if new_state_option.is_some() {
-                self.state = new_state_option.unwrap();
-
-                return true;
-            }
-        }
-
-        return false;
-    }
+    let state_machine = BottleFiller::Machine::new();
 }
